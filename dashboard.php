b7906db73452b97e82a6e908c3e9eaaba67fc44f
ayunur14 <ayuindahsari988@gmail.com>
2025-11-08 09:35:22 +0700

    ayu push code perubahan

--- dashboard.php
+++ dashboard.php
@@ -58,7 +58,7 @@ if ($role === 'admin' || $role === 'gudang') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - <?php echo APP_NAME; ?></title>
+    <title>Dashboard - MarsyellaAyu <?php echo APP_NAME; ?></title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
